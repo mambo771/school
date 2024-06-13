2d58c1a88da7adecc42e9fd983f3fef569e30bb7
origin <mambo771@mail.ru>
2024-06-13 19:59:02 +0300

    Initial commit

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -56,7 +56,7 @@ public class FacultyController {
         Collection<Faculty> faculties= service.getAll();
         return ResponseEntity.ok(faculties);
     }
-    @GetMapping("students/{facultyId}")
+    @GetMapping("students/{facultyid}")
     @Operation (summary = "Получение студентов факультета")
     public ResponseEntity<Collection<Student>> getStudentsOfFaculty(@PathVariable Long facultyId){
         Collection<Student> students= service.getStudents(facultyId);
